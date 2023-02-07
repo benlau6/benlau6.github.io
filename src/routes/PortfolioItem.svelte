4b221d3b7da6017ff5e328dc15abd9dd77be87ea
benlau6 <benyplau@gmail.com>
2023-02-07 19:04:31 +0800

    Add content

---
+++ src/routes/PortfolioItem.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let title: string;
+	export let href: string;
+	export let imgSrc: string;
+</script>
+
+<form method="get" action={href}>
+	<span>{title}</span>
+	<button type="submit" class="card" style="background-image: url({imgSrc});" />
+</form>
+
+<style>
+	span {
+		margin-left: 4px;
+		font-size: 1.25rem;
+		text-transform: capitalize;
+	}
+	.card {
+		background-color: white;
+		border-radius: 8px;
+		width: 100%;
+		height: 100%;
+		box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.1);
+		padding: 1rem;
+		cursor: pointer;
+		background-size: contain;
+		/* background-size: cover; */
+		background-repeat: no-repeat;
+		background-position: center;
+	}
+</style>
---
+++ src/routes/portfolio_items.ts
@@ -0,0 +1,50 @@
+export const stats_items = [
+    {
+        title: 'Change point Detection',
+        href: 'https://github.com/benlau6/hierarchy-bayesian-modeling-time-series-sensor/blob/main/demonstration.ipynb',
+        imgSrc: 'images/change_point.png'
+    },
+    {
+        title: 'Kalman Filter',
+        href: 'https://github.com/benlau6/hierarchy-bayesian-modeling-time-series-sensor/blob/main/kalman_filter.ipynb',
+        imgSrc: 'images/kalman_filter.png'
+    },
+    {
+        title: 'Gaussian Process',
+        href: 'https://github.com/benlau6/hierarchy-bayesian-modeling-time-series-sensor/blob/main/gaussian_process.ipynb',
+        imgSrc: 'images/gaussian_process.png'
+    },
+    {
+        title: 'Forcasting Time Series',
+        href: 'https://github.com/benlau6/amazon-forecast-pipeline/blob/main/Evaluation.ipynb',
+        imgSrc: 'images/forecast.png'
+    },
+    {
+        title: 'Build a Transformer From Scratch',
+        href: 'https://github.com/benlau6/transformers-jax/blob/main/transformers_jax.ipynb',
+        imgSrc: 'images/transformer.png'
+    },
+    {
+        title: 'Build a State-Space Model from scratch',
+        href: 'https://colab.research.google.com/github/benlau6/probml-notebooks/blob/discretized-ssm-patch/notebooks/discretized_ssm.ipynb',
+        imgSrc: 'images/ssm.png'
+    }
+];
+
+export const web_items = [
+    {
+        title: 'Source code of this website',
+        href: 'https://github.com/benlau6/benlau6.github.io',
+        imgSrc: 'images/sveltekit.webp'
+    },
+    {
+        title: 'Fullstack Web Development',
+        href: 'https://github.com/benlau6/fastapi-fullstack',
+        imgSrc: 'images/fastapi.png'
+    },
+    {
+        title: 'Web Scraping',
+        href: 'https://github.com/benlau6/sfc-crawler',
+        imgSrc: 'images/scrapy.png'
+    }
+];
