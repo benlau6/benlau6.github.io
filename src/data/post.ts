4f137da6a67cf0c66f6672ee7186c0c744f53122
benlau6 <benyplau@gmail.com>
2024-09-17 19:00:54 +0800

    fix: correct typing error due to collection renaming

--- src/data/post.ts
+++ src/data/post.ts
@@ -9,14 +9,14 @@ export async function getAllPosts(collection: "blog" | "projects" = "blog") {
 }
 
 /** returns the date of the post based on option in siteConfig.sortPostsByUpdatedDate */
-export function getPostSortDate(post: CollectionEntry<"blog">) {
+export function getPostSortDate(post: CollectionEntry<"blog" | "projects">) {
   return siteConfig.sortPostsByUpdatedDate && post.data.updatedDate !== undefined
     ? new Date(post.data.updatedDate)
     : new Date(post.data.publishDate);
 }
 
 /** sort post by date (by siteConfig.sortPostsByUpdatedDate), desc.*/
-export function sortMDByDate(posts: CollectionEntry<"blog">[]) {
+export function sortMDByDate(posts: CollectionEntry<"blog" | "projects">[]) {
   return posts.sort((a, b) => {
     const aDate = getPostSortDate(a).valueOf();
     const bDate = getPostSortDate(b).valueOf();
@@ -27,8 +27,8 @@ export function sortMDByDate(posts: CollectionEntry<"blog">[]) {
 /** groups posts by year (based on option siteConfig.sortPostsByUpdatedDate), using the year as the key
  *  Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so.
  */
-export function groupPostsByYear(posts: CollectionEntry<"blog">[]) {
-  return posts.reduce<Record<string, CollectionEntry<"blog">[]>>((acc, post) => {
+export function groupPostsByYear(posts: CollectionEntry<"blog" | "projects">[]) {
+  return posts.reduce<Record<string, CollectionEntry<"blog" | "projects">[]>>((acc, post) => {
     const year = getPostSortDate(post).getFullYear();
     if (!acc[year]) {
       acc[year] = [];
@@ -41,21 +41,21 @@ export function groupPostsByYear(posts: CollectionEntry<"blog">[]) {
 /** returns all tags created from posts (inc duplicate tags)
  *  Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so.
  *  */
-export function getAllTags(posts: CollectionEntry<"blog">[]) {
+export function getAllTags(posts: CollectionEntry<"blog" | "projects">[]) {
   return posts.flatMap((post) => [...post.data.tags]);
 }
 
 /** returns all unique tags created from posts
  *  Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so.
  *  */
-export function getUniqueTags(posts: CollectionEntry<"blog">[]) {
+export function getUniqueTags(posts: CollectionEntry<"blog" | "projects">[]) {
   return [...new Set(getAllTags(posts))];
 }
 
 /** returns a count of each unique tag - [[tagName, count], ...]
  *  Note: This function doesn't filter draft posts, pass it the result of getAllPosts above to do so.
  *  */
-export function getUniqueTagsWithCount(posts: CollectionEntry<"blog">[]): [string, number][] {
+export function getUniqueTagsWithCount(posts: CollectionEntry<"blog" | "projects">[]): [string, number][] {
   return [
     ...getAllTags(posts).reduce(
       (acc, t) => acc.set(t, (acc.get(t) ?? 0) + 1),
